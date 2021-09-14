d711910558c08f547e6f7e7f77c738a992241009
Amir <chikchicly@gmail.com>
2021-09-14 15:49:28 +0700

    delite 5

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-var app5 = new Vue({
-  el: '#app-5',
-  data: {
-    message: 'Привет, Vue.js!'
-  },
-  methods: {
-    reverseMessage: function () {
-      this.message = this.message.split('').reverse().join('')
-    }
-  }
-}),
 
 var app4 = new Vue({
   el: '#app-4',
