0fc44ae7322e903186123302670c3ddb7db3e487
Amir <chikchicly@gmail.com>
2021-09-14 15:22:47 +0700

    ok

--- script.js
+++ script.js
@@ -24,19 +24,4 @@ var app6 = new Vue({
   data: {
     messages: 'Привет, Vue!'
   }
-}),
-Vue.component('todo-item', {
-  props: ['todo'],
-  template: '<li>{{ todo.text }}</li>'
-}),
-
-var app7 = new Vue({
-  el: '#app-7',
-  data: {
-    groceryList: [
-      { id: 0, text: 'Овощи' },
-      { id: 1, text: 'Сыр' },
-      { id: 2, text: 'Что там ещё люди едят?' }
-    ]
-  }
 })
