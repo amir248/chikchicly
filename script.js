dca3d515968f626ff076a499aa50991acf46d03e
Amir <chikchicly@gmail.com>
2021-09-14 16:01:34 +0700

    ok

--- script.js
+++ script.js
@@ -1,29 +1,6 @@
-var app5 = new Vue({
-  el: '#app-5',
-  data: {
-    message: 'Привет, Vue.js!'
-  },
-  methods: {
-    reverseMessage: function () {
-      this.message = this.message.split('').reverse().join('')
-    }
-  }
-})
-
-var app4 = new Vue({
-  el: '#app-4',
-  data: {
-    todos: [
-      { text: 'Изучить JavaScript' },
-      { text: 'Изучить Vue' },
-      { text: 'Создать что-нибудь классное' }
-    ]
-  }
-})
-
 var app6 = new Vue({
   el: '#app-6',
   data: {
-    messages: 'Привет, Vue!'
+    message: 'Привет, Vue!'
   }
 })
