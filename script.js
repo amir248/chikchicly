2585fd2153c264cea69fce5832c5b0a586bdd6a1
Amir <chikchicly@gmail.com>
2021-09-14 15:20:01 +0700

    седьмой компонент который приходит в общую кучу

--- script.js
+++ script.js
@@ -25,3 +25,18 @@ var app6 = new Vue({
     messages: 'Привет, Vue!'
   }
 })
+Vue.component('todo-item', {
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+})
+
+var app7 = new Vue({
+  el: '#app-7',
+  data: {
+    groceryList: [
+      { id: 0, text: 'Овощи' },
+      { id: 1, text: 'Сыр' },
+      { id: 2, text: 'Что там ещё люди едят?' }
+    ]
+  }
+})
