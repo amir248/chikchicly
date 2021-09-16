8f33c21664f35ce55ad2e9439c5827a7de637bae
Amir <chikchicly@gmail.com>
2021-09-16 09:58:25 +0700

    ``

--- site/script.js
+++ site/script.js
@@ -10,4 +10,4 @@ document.querySelector('#go').addEventListener('click',()=>{
 });
 alert("Привет Уважаемый:"+'_'+ localStorage.getItem('name') );
 let nameUser= localStorage.getItem('name');
-document.querySelector('#nameUser').innerHTML=localStorage.getItem('name');
+document.querySelector('#nameUser').innerHTML=`localStorage.getItem('name')`;
