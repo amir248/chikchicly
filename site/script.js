8ed7356da75a2d8fb191b006fa8aef272241e613
Amir <chikchicly@gmail.com>
2021-09-16 09:41:44 +0700

    obj

--- site/script.js
+++ site/script.js
@@ -5,7 +5,7 @@ var app6 = new Vue({
   }
 })
 document.querySelector('#go').addEventListener('click',()=>{
-  localStorage.setItem('name', "app6.message");
+  localStorage.setItem('name', app6.message);
   console.log('clickButton');
 });
 alert( localStorage.getItem('name') );
