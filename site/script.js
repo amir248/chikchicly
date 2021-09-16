721159cb9b45f6a7b3a2340e9ad8be1a8a02ec7c
Amir <chikchicly@gmail.com>
2021-09-16 11:18:47 +0700

    promt

--- site/script.js
+++ site/script.js
@@ -13,7 +13,7 @@ if(localStorage.getItem('name')!=null){
 alert("Привет Уважаемый:"+'_'+ localStorage.getItem('name') );
 }else{
   console.log('getItemOfnull');
-  promt(Пожалуйста, будьте добра представьтесь.[localStorage.setItem('name,app6.messge')]);
+  // promt(Пожалуйста, будьте добра представьтесь.[localStorage.setItem('name,app6.messge')]);
 }
 let nameUser= localStorage.getItem('name');
 document.querySelector('#nameUser').innerHTML=`${localStorage.getItem('name')}`;
