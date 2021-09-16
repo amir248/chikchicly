d083f4a20ac75ad84105c091415abae9ea81f613
Amir <chikchicly@gmail.com>
2021-09-16 10:05:59 +0700

    of_neNull

--- site/script.js
+++ site/script.js
@@ -8,7 +8,11 @@ document.querySelector('#go').addEventListener('click',()=>{
   localStorage.setItem('name', app6.message);
   console.log('clickButton');
 });
+if(locanStorage.getItem('name')!=null){
 alert("Привет Уважаемый:"+'_'+ localStorage.getItem('name') );
+}else{
+  console.log(getItemOfnull);
+}
 let nameUser= localStorage.getItem('name');
 // document.querySelector('#nameUser').innerHTML=`localStorage.getItem('name')`;
 document.querySelector('#nameUser').innerHTML=`${nameUser}`;
