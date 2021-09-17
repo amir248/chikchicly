b1fa9af6513e13c5ef94ae293cce114bf6646cba
Amir <chikchicly@gmail.com>
2021-09-17 09:36:38 +0700

    commercial

--- site/script.js
+++ site/script.js
@@ -12,6 +12,9 @@ document.querySelector('#go').addEventListener('click',()=>{
 if(localStorage.getItem('name')!=null){
   if(window.location.hash=='#hero-webMaster'){
     console.log("ok location.hash(hero-webMaster)");
+  }else if(window.location.hash=='#commercial-offer'){
+    console.log("ok location.hash(commercial-offer)");{
+
   }else{
     alert("Привет Уважаемый:"+'_'+ localStorage.getItem('name') );
   }
